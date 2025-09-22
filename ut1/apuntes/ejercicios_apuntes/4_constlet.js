bfba3d1169ec5fa142eb0d6536f0e44c103c5baa
PeterMartEsc <petermartesc@gmail.com>
2025-09-22 20:24:00 +0100

    feat(ejercicios_apuntes): se añaden 2 ejercicios de los apuntes de js

---
+++ ut1/apuntes/ejercicios_apuntes/4_constlet.js
@@ -0,0 +1,7 @@
+let colorFavorito = "azul";
+const comidaFavorita = "pizza";
+
+colorFavorito = "verde";
+
+console.log("Color favorito: " + colorFavorito);
+console.log("Comida favorita: " + comidaFavorita);
---
+++ ut1/apuntes/ejercicios_apuntes/5_asignaciones.js
@@ -0,0 +1,8 @@
+let x = 3;
+console.log(x);
+// Caso 2: Operador de asignación con suma (x = x + 2)
+x += 2;
+console.log(x);
+// Caso 3: Asignación con operador unario positivo (equivale a x = +2)
+x =+ 2;
+console.log(x);
