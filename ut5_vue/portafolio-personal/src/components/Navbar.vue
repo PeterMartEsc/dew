92eb1bf8c5b30c38a2217ce57b10beef5ebf30bf
PeterMartEsc <petermartesc@gmail.com>
2025-12-17 17:15:59 +0000

    feat(portafolios): se empieza la tarea del portafolios

---
+++ ut5_vue/portafolio-personal/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <span class="link-navbar">            
+            <router-link to="/">Home</router-link>
+        </span>
+        <span>        
+            <router-link to="/about-me">About</router-link>
+        </span>
+</nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+    .link-navbar{
+        margin-right: 10px;
+    }
+</style>
---
+++ ut5_vue/portafolio-personal/src/navigation/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <Navbar/>
+    <div>
+        Esta es la página principal
+    </div>
+</template>
+
+<script setup lang="ts">
+    import Navbar from '../components/Navbar.vue';
+</script>
+
+<style scoped>
+
+</style>
